840aa6a32a8179e7eca331a57a7ffa484fddfe93
Ivan Platunov <vanya.platun@yandex.ru>
2023-12-21 18:28:12 +0300

    work 100%

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Проектная работа Mesto
+# Проект: [Mesto](https://github.com/vnn-ktt/mesto-project-ff.git)
 
+## Данный репозиторий является частью проекта "Mesto" (ссылка недоступна)!
+
+## Описание
+
+В этом проекте представлена часть веб-сайта!
+
+## Технологии
+
+- Native HTML, SVG
+- Native CSS: Flexbox and Grid Layout
+- Native JavaScript
+- Adaptive design
+- Progressive improvement
+
+## Скриншот
+
+![Скриншот](https://github.com/vnn-ktt/mesto-project-ff/assets/106499823/2d43c999-40f7-44c5-b01e-e1369309bb40)
+
+## Ссылка на репозиторий
+
+[Mesto](https://github.com/vnn-ktt/mesto-project-ff.git)
+
+## Контакты
+
+- Телеграм: <https://t.me/vnn_ktt>
+- E-mail: <vanya.platun@yadnex.ru>
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+// DOM-узлы
+const pageBody = document.querySelector('.page');
+const cardsList = pageBody.querySelector('.places__list');
+const cardTemplate = pageBody.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// Функции
+const makeCard = function (data, deleteCard) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__title').textContent = data.name;
+  const cardImage = card.querySelector('.card__image');
+  cardImage.alt = data.name;
+  cardImage.src = data.link;
+  const cardDeleteButton = card.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', () => deleteCard(card));
+  return card;
+};
+const deleteCard = (card) => card.remove();
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// Инициализация страницы
+initialCards.forEach((elem) => {
+  cardsList.append(makeCard(elem, deleteCard));
+});
